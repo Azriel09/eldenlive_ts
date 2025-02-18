2c67d030386b92d835c81d6b8d50801456141530
Azriel09 <jhanmarmel@gmail.com>
2025-02-18 17:18:37 +0800

    icons and dropdown menu in sidebar

--- src/components/NavSideBar/NavSideBar.tsx
+++ src/components/NavSideBar/NavSideBar.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import Typography from "@mui/material/Typography";
 import {
   Sidebar,
   Menu,
   MenuItem,
+  SubMenu,
   MenuItemStyles,
   ElementStyles,
+  menuClasses,
 } from "react-pro-sidebar";
-
+import styles from "./NavSideBar.module.css";
+import { TalentIconByName } from "./icons";
 interface MenuItemStyles {
   root?: ElementStyles;
   button?: ElementStyles;
@@ -20,19 +23,29 @@ interface MenuItemStyles {
   SubMenuExpandIcon?: ElementStyles;
 }
 
+const icons: string[] = ["ame", "gura", "irys", "calli", "ina", "kronii"];
+const talentNames: string[] = [
+  "Amelia Watson",
+  "Gawr Gura",
+  "IRyS",
+  "Mori Calliope",
+  "Ninomae Ina'nis",
+  "Ouro Kronii",
+];
+
 export default function SideBarNav() {
   const [toggled, setToggled] = useState<boolean>(true);
   return (
     <>
       <Sidebar
         backgroundColor="rgba(28, 29, 33, 1)"
-        toggled={false}
+        toggled={true}
         breakPoint="all"
         width="350px"
       >
         <Menu
           menuItemStyles={{
-            button: ({ level, active, disabled }) => {
+            button: () => {
               return {
                 color: "#b9b9bb",
                 backgroundColor: "rgba(28, 29, 33, 1)",
@@ -47,7 +60,36 @@ export default function SideBarNav() {
           }}
         >
           <MenuItem disabled></MenuItem>
-          <MenuItem disabled></MenuItem>
+          <MenuItem disabled></MenuItem>{" "}
+          <Typography className={styles.typography}>
+            Death Timestamps
+          </Typography>
+          <SubMenu
+            label="Hololive EN"
+            rootStyles={{
+              ["& > ." + menuClasses.button]: {
+                backgroundColor: "#1c1d21",
+                color: "#b9b9bb",
+                letterSpacing: "0.5px",
+              },
+
+              ["." + menuClasses.subMenuContent]: {
+                backgroundColor: "#1c1d21",
+              },
+            }}
+          >
+            {icons.map((talent, index) => {
+              return (
+                <MenuItem
+                  key={talent}
+                  onClick={() => setToggled(!toggled)}
+                  icon={<TalentIconByName name={talent} />}
+                >
+                  {talentNames[index]}
+                </MenuItem>
+              );
+            })}
+          </SubMenu>
         </Menu>
       </Sidebar>
       <Outlet />
---
+++ src/components/NavSideBar/icons.tsx
@@ -0,0 +1,90 @@
+import GuraPic from "../../assets/talent-icons/gura.png";
+import AmePic from "../../assets/talent-icons/ame.png";
+import CalliPic from "../../assets/talent-icons/calli.png";
+import InaPic from "../../assets/talent-icons/ina.png";
+import IrysPic from "../../assets/talent-icons/irys.png";
+import KroniiPic from "../../assets/talent-icons/kronii.png";
+// import OkayuPic from "../images/talent-icons/okayu.png";
+// import FlarePic from "../images/talent-icons/flare.png";
+// import LuiPic from "../images/talent-icons/lui.png";
+// import LunaPic from "../images/talent-icons/luna.png";
+// import MikoPic from "../images/talent-icons/miko.png";
+// import ChloePic from "../images/talent-icons/chloe.png";
+// import KoyoriPic from "../images/talent-icons/koyori.png";
+// import LamyPic from "../images/talent-icons/lamy.png";
+// import BotanPic from "../images/talent-icons/botan.png";
+// import PekoraPic from "../images/talent-icons/pekora.png";
+// import NoelPic from "../images/talent-icons/noel.png";
+// import AkirosePic from "../images/talent-icons/aki.png";
+// import MoonaPic from "../images/talent-icons/moona.png";
+// import ReinePic from "../images/talent-icons/reine.png";
+interface TalentIconProps {
+  src: string;
+  alt: string;
+}
+interface TalentIconByNameProps {
+  name: string;
+}
+type TalentName = "gura" | "ame" | "calli" | "ina" | "irys" | "kronii";
+
+const TALENT_ICONS: Record<TalentName, string> = {
+  gura: GuraPic,
+  ame: AmePic,
+  calli: CalliPic,
+  ina: InaPic,
+  irys: IrysPic,
+  kronii: KroniiPic,
+};
+
+const TalentIcon = ({ src, alt }: TalentIconProps) => (
+  <img src={src} alt={alt} style={{ borderRadius: "50%", width: "35px" }} />
+);
+export const TalentIconByName = ({ name }: TalentIconByNameProps) => (
+  <TalentIcon src={TALENT_ICONS[name.toLowerCase() as TalentName]} alt={name} />
+);
+
+// export function OkayuIcon() {
+//   return <img src={OkayuPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+
+// export function FlareIcon() {
+//   return <img src={FlarePic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function LuiIcon() {
+//   return <img src={LuiPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function LunaIcon() {
+//   return <img src={LunaPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function MikoIcon() {
+//   return <img src={MikoPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function ChloeIcon() {
+//   return <img src={ChloePic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function KoyoriIcon() {
+//   return <img src={KoyoriPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function ReineIcon() {
+//   return <img src={ReinePic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function LamyIcon() {
+//   return <img src={LamyPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function BotanIcon() {
+//   return <img src={BotanPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function PekoraIcon() {
+//   return <img src={PekoraPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function NoelIcon() {
+//   return <img src={NoelPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
+// export function AkiroseIcon() {
+//   return (
+//     <img src={AkirosePic} style={{ borderRadius: "50%", width: "35px" }} />
+//   );
+// }
+// export function MoonaIcon() {
+//   return <img src={MoonaPic} style={{ borderRadius: "50%", width: "35px" }} />;
+// }
