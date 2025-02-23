be767bbe3e7b888645883b767639048efacb8d58
Azriel09 <jhanmarmel@gmail.com>
2025-02-24 00:45:09 +0800

    timestamps now showing

--- src/components/Timestamps/TimestampsSlider.tsx
+++ src/components/Timestamps/TimestampsSlider.tsx
@@ -1,30 +1,52 @@
 import { Slider } from "@mui/material";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelectedTalent } from "../../context/TalentContext";
 import moment from "moment";
 interface TimestampsSliderProps {
   data: Record<string, any>;
   selectedStream: string;
 }
+interface SliderData {
+  value: number;
+}
 const YT_API = import.meta.env.VITE_YOUTUBE_API_KEY;
 export default function TimestampsSlider({
   data,
   selectedStream,
 }: TimestampsSliderProps) {
+  const ref = React.createRef();
   const { selectedTalent } = useSelectedTalent();
-  const [enemies, setEnemies] = useState<string[]>();
-  const [timestamps, setTimestamps] = useState<string[]>();
+  const [enemies, setEnemies] = useState<string[] | undefined>();
+
   const [max, setMax] = useState<number>();
-
+  const [sliderData, setSliderData] = useState<SliderData[]>();
+  const [npc, setNPC] = useState<boolean>(false);
+  const [boss, setBoss] = useState<boolean>(false);
   useEffect(() => {
-    setEnemies(data[selectedTalent][selectedStream]["enemies"]);
-    setTimestamps(data[selectedTalent][selectedStream]["timestamps"]);
+    const temp_e: string[] = data[selectedTalent][selectedStream]["enemies"];
+    const temp_t: string[] = data[selectedTalent][selectedStream]["timestamps"];
+    const temp_a: [] = [];
 
+    // Converting each timestamps into total seconds
+    temp_t.map((t) => {
+      const tempo: { value: number } = {value: 0};
+      let a = t.split(":");
+      const totalSeconds = +a[0] * 60 * 60 + +a[1] * 60 + +a[2];
+      tempo.value = Number(totalSeconds);
+
+      temp_a.push(tempo);
+    });
+
+    setEnemies(temp_e);
+    setSliderData(temp_a);
+
+    // Getting the video ID
     const url = selectedStream.replace("watch?v=", "embed/");
     const id = url.split("/").pop();
     getVideoDuration(id);
   }, [selectedStream]);
-  const getVideoDuration = async (id) => {
+
+  const getVideoDuration = async (id: string) => {
     // Via Youtube API
     const url = `https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&key=${YT_API}`;
 
@@ -33,66 +55,101 @@ export default function TimestampsSlider({
       const data = await response.json();
       const iso8601Duration = data.items[0].contentDetails.duration;
       const secondsDuration = moment.duration(iso8601Duration).asSeconds();
-      console.log(secondsDuration);
+
       setMax(secondsDuration);
     } catch (error) {
       console.error(error);
     }
   };
+  function valueLabelFormat(value: number) {
+    
+    let index = sliderData.findIndex((mark) => mark.value === value);
+    console.log(value)
+    try {
+      if (enemies[index].includes("Boss")) {
+        const death = enemies[index].replace("Boss", "");
 
+        return death;
+      } else if (enemies[index].includes("NPC")) {
+        const death = enemies[index].replace("NPC", "");
+        return death;
+      } else {
+        return enemies[index];
+      }
+    } catch (err) {}
+  }
+  function valuetext(value) {
+    return;
+  }
+  const checkBoss = (e) => {
+    const index = sliderData.findIndex((mark) => mark.value === e.target.value);
+    ref.current.seekTo(e.target.value - 2);
+    if (enemies[index].includes("Boss")) {
+      setBoss(true);
+      setNPC(false);
+    } else if (enemies[index].includes("NPC")) {
+      setNPC(true);
+      setBoss(false);
+    } else {
+      setBoss(false);
+      setNPC(false);
+    }
+  };
   return (
     <div>
       {" "}
-      <Slider
-        aria-label="Restricted values"
-        valueLabelFormat={valueLabelFormat}
-        getAriaValueText={valuetext}
-        valueLabelDisplay="on"
-        step={null}
-        min={0}
-        max={max}
-        onChange={(e) => checkBoss(e)}
-        marks={sliderData}
-        track={false}
-        sx={[
-          {
-            color: "rgba(0,0,0,0)",
-            // backgroundColor: "#323233",
-            width: "98%",
+      {sliderData &&  (
+        <Slider
+          aria-label="Restricted values"
+          valueLabelFormat={valueLabelFormat}
+          getAriaValueText={valuetext}
+          valueLabelDisplay="on"
+          step={null}
+          min={0}
+          max={max}
+          onChange={(e) => checkBoss(e)}
+          marks={sliderData}
+          track={false}
+          sx={[
+            {
+              color: "rgba(0,0,0,0)",
+              // backgroundColor: "#323233",
+              width: "98vw",
 
-            "& .MuiSlider-mark": {
-              backgroundColor: "red",
-              height: "17px",
-              width: "1px",
-              borderRadius: "1px",
-              "&:hover": {
-                width: "2px",
-                height: "20px",
+              "& .MuiSlider-mark": {
+                backgroundColor: "red",
+                height: "17px",
+                width: "1px",
+                borderRadius: "1px",
+                "&:hover": {
+                  width: "2px",
+                  height: "20px",
+                },
+              },
+              "& .MuiSlider-thumb": {
+                color: "#b9b9bb",
+                height: 25,
+                width: "3px",
+              },
+              "& .MuiSlider-valueLabel": {
+                backgroundColor: "gray",
               },
             },
-            "& .MuiSlider-thumb": {
-              color: "#b9b9bb",
-              height: 25,
-              width: "3px",
+            boss && {
+              "& .MuiSlider-valueLabel": {
+                backgroundColor: "lightblue",
+                color: "black",
+              },
             },
-            "& .MuiSlider-valueLabel": {
-              backgroundColor: "gray",
+            npc && {
+              "& .MuiSlider-valueLabel": {
+                backgroundColor: "green",
+                color: "white",
+              },
             },
-          },
-          boss && {
-            "& .MuiSlider-valueLabel": {
-              backgroundColor: "lightblue",
-              color: "black",
-            },
-          },
-          npc && {
-            "& .MuiSlider-valueLabel": {
-              backgroundColor: "green",
-              color: "white",
-            },
-          },
-        ]}
-      />
+          ]}
+        />
+      ) }
     </div>
   );
 }
--- src/components/Timestamps/VideoPlayer.tsx
+++ src/components/Timestamps/VideoPlayer.tsx
@@ -1,22 +1,18 @@
 import LoadingComponent from "../global/Loading";
+import React from "react";
 import ReactPlayer from "react-player";
 interface VideoPlayerProps {
   selectedStream: string;
 }
 const YT_API = import.meta.env.VITE_YOUTUBE_API_KEY;
 export default function VideoPlayer({ selectedStream }: VideoPlayerProps) {
+  const ref = React.createRef();
   return (
     <>
       {!selectedStream ? (
         <LoadingComponent />
       ) : (
-        <ReactPlayer
-        playing
-          controls
-          url={selectedStream}
-          width={"95vw"}
-       
-        />
+        <ReactPlayer ref={ref} playing controls url={selectedStream} width={"95vw"} />
       )}
     </>
   );
